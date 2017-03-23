c23d3c1d5ed08ab1f746de63f176a52d7fb472d0
Jason Laster <jason.laster.11@gmail.com>
2017-03-23 17:03:09 -0400

    prettify

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-var cp = require('child_process'),
-    fs = require('fs'),
-    path = require('path');
+var cp = require('child_process'), fs = require('fs'), path = require('path');
 
-var bootstrap =
-  fs.readFileSync(path.join(__dirname, 'bootstrap.js'), 'utf8');
+var bootstrap = fs.readFileSync(path.join(__dirname, 'bootstrap.js'), 'utf8');
 
 module.exports = Worker;
 function Worker(file, type) {
-  if(type === true) {
-    type = "eval";
+  if (type === true) {
+    type = 'eval';
   } else {
-    type = "require";
+    type = 'require';
   }
 
   var self = this;
@@ -21,16 +18,16 @@ function Worker(file, type) {
   }
 
   this.child.send(file);
-  this.child.on('message', function (msg) {
+  this.child.on('message', function(msg) {
     var parsed = JSON.parse(msg);
     self.onmessage && self.onmessage.call(self, parsed);
-    if(self.handlers['message']) {
+    if (self.handlers['message']) {
       self.handlers['message'].forEach(func => func.call(self, parsed));
     }
   });
-  this.child.on('error', function (err) {
+  this.child.on('error', function(err) {
     self.onerror && self.onerror(err);
-    if(self.handlers['error']) {
+    if (self.handlers['error']) {
       self.handlers['error'].forEach(func => func.call(self, err));
     }
   });
@@ -38,15 +35,15 @@ function Worker(file, type) {
   this.handlers = {};
 }
 
-Worker.prototype.postMessage = function (msg) {
-  this.child.send(JSON.stringify({ data: msg }));
+Worker.prototype.postMessage = function(msg) {
+  this.child.send(JSON.stringify({data: msg}));
 };
 
 Worker.prototype.terminate = function() {
   this.child.kill();
 };
 
-Worker.prototype.addEventListener = function (eventName, cb) {
+Worker.prototype.addEventListener = function(eventName, cb) {
   if (!this.handlers[eventName]) {
     this.handlers[eventName] = [];
   }
@@ -55,7 +52,7 @@ Worker.prototype.addEventListener = function (eventName, cb) {
 
 Worker.prototype.removeEventListener = function(eventName, cb) {
   const eventHandlers = this.handlers[eventName];
-  if(eventHandlers) {
+  if (eventHandlers) {
     this.handlers[eventName] = eventHandlers.filter(func => func !== cb);
   }
-}
+};
--- requireworker.js
+++ requireworker.js
@@ -1,44 +1,43 @@
 var fs = require('fs');
-process.once('message', function (modulePath) {
-  var noop = function () { };
+process.once('message', function(modulePath) {
+  var noop = function() {};
   global.self = {
-    postMessage: function (msg) {
-      process.send(JSON.stringify({ data: msg }));
+    postMessage: function(msg) {
+      process.send(JSON.stringify({data: msg}));
     },
     onmessage: noop,
     onerror: noop,
-    addEventListener: function (eventName, cb) {
+    addEventListener: function(eventName, cb) {
       if (eventName === 'message') {
-        global.onmessage = global.self.onmessage = cb;
+        global.onmessage = (global.self.onmessage = cb);
       } else if (eventName === 'error') {
-        global.onerror = global.self.onerror = cb;
+        global.onerror = (global.self.onerror = cb);
       }
-    }
+    },
   };
 
-  global.importScripts = function () {
+  global.importScripts = function() {
     var fs = require('fs');
     var path = require('path');
     var files = Array.prototype.slice.call(arguments);
-    var script =
-      files
-        .map(function (file) {
-          return fs.readFileSync(file, 'utf8');
-        })
-        .join('\n');
+    var script = files
+      .map(function(file) {
+        return fs.readFileSync(file, 'utf8');
+      })
+      .join('\n');
     var vm = require('vm');
     var script = vm.createScript(script);
     script.runInThisContext();
   };
 
-  Object.keys(global.self).forEach(function (key) {
+  Object.keys(global.self).forEach(function(key) {
     global[key] = global.self[key];
   });
 
-  process.on('message', function (msg) {
+  process.on('message', function(msg) {
     global.self.onmessage && global.self.onmessage(JSON.parse(msg));
   });
-  process.on('error', function (err) {
+  process.on('error', function(err) {
     global.self.onerror && global.self.onerror(err);
   });
 
